2ce7d96da1739640f04c425ec8efdd2e45f7e794
dschadlich <dschadlich@gmail.com>
2016-03-08 10:03:32 -0500

    creating gh-pages branch

---
+++ slideShow.js
@@ -0,0 +1,107 @@
+var doRandom = false;
+var duration = 5000; //time image is shown in milliseconds
+var firstImage = 1;
+var ImageCount = 8;
+var Timer;
+var slides = new Array ();
+
+$( document ).ready(function() {
+	for (i = firstImage; i <=ImageCount; i++) { 
+//		$( "#slideShow" ). append ('<img src="images/slide'+i+'.jpg" alt="" id="slide'+i+'" class="slideShow_image" />');//fill the div with all the images
+	}
+//var slideshow = $( "#slideShow" );
+
+$('img', $('#slideShow')).each(function () {
+   // console.log($(this).height()); //log every element found to console output
+});
+
+
+//console.log();
+
+
+initSlideShow ("slideShow");
+
+
+
+});
+
+
+function initSlideShow (containerID){
+
+	var i = 0;
+	var maxHeight =0;
+
+	$('img', $('#'+ containerID)).each(function () {
+		//		console.log($(this).height()); //log every element found to console output
+		slides[i] = $(this);
+		i++;
+		if ($(this).height() > maxHeight){
+			maxHeight =$(this).height(); 
+		}
+
+	});
+	updateMaxHeight (containerID);
+	updateSlideShow (0, slides.length - 1);
+}
+
+function updateMaxHeight (containerID){
+	var maxHeight =0;
+	$('img', $('#'+ containerID)).each(function () {
+		//		console.log($(this).height()); //log every element found to console output
+
+		if ($(this).height() > maxHeight){
+			maxHeight =$(this).height(); 
+		}
+
+	});
+
+  $( "#" + containerID).css ("height", maxHeight + "px");
+
+
+}
+
+$( window ).resize(function() {
+  //$( "#log" ).append( "<div>Handler for .resize() called.</div>" );
+updateMaxHeight ("slideShow");
+});
+
+
+
+function updateSlideShow (slideNumber, previous){
+	console.log ("parameters: " +  slideNumber + ":" + previous);
+ // $( "slide"+ slideNumber ). css ('<img src="images/slide'+slideNumber+'.jpg" alt="" class="slideShow_image" />');
+ // $( "#slide"+ previous ).css ("opacity", 0);
+//  $( "#slide"+ slideNumber ).css ("opacity", 1);
+//$( "#slideShow" ).css ("height",  $( "#slide"+ slideNumber ).height() + "px");
+console.log (slides [slideNumber]);
+slides [slideNumber].css ("opacity", 1);
+slides [previous].css ("opacity", 0);
+
+
+
+console.log ("hieght: " + $( "#slide"+ slideNumber ).height() );
+
+
+
+    console.log ("updateSlideShow: " +slideNumber + ":" + previous );
+	previous = slideNumber;
+  if (doRandom){
+	slideNumber = Math.floor((Math.random() * ImageCount) + firstImage);  
+  }else{
+
+    if (slideNumber == slides.length - 1){
+      slideNumber = firstImage;
+    }else{
+      slideNumber ++;
+    }
+
+  }
+    Timer = setTimeout (updateSlideShow, duration, slideNumber, previous);
+}
+
+
+function nextSlide (){
+
+}
+
+
